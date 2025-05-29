cf91d6735dab902c71b77e1f88a7f87402d0e902
Ashwin <Ashwin@vridhee.com>
2025-05-29 23:51:18 +0530

    format

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useRef, useState } from "react";
-import './App.css'
+import "./App.css";
 import EmployeeForm from "./Component/EmployeeForm";
 
 export default function App() {
- const [theme,setTheme]=useState("light")
+  const [theme, setTheme] = useState("light");
 
-useEffect(()=>{
-document.documentElement.setAttribute("data-Theme",theme)
-},[theme])
+  useEffect(() => {
+    document.documentElement.setAttribute("data-Theme", theme);
+  }, [theme]);
 
-const changeTheme=()=>{
-  setTheme(prev=>prev=="light"?"dark":"light")
-}
+  const changeTheme = () => {
+    setTheme((prev) => (prev == "light" ? "dark" : "light"));
+  };
 
   return (
-  <>
-  <h1 style={{textAlign:"Center"}}>Emplyoee Onbording</h1>
-   <div style={{display:"flex",justifyContent:"end",alignItems:"Center",height:"50px",paddingRight:"40px"}}><button onClick={changeTheme}>{theme==="light"?"Dark":"Light"}</button></div>
-<EmployeeForm/>
-
-  </>
+    <>
+      <h1 style={{ textAlign: "Center" }}>Emplyoee Onbording</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+          alignItems: "Center",
+          height: "50px",
+          paddingRight: "40px",
+        }}
+      >
+        <button onClick={changeTheme}>
+          {theme === "light" ? "Dark" : "Light"}
+        </button>
+      </div>
+      <EmployeeForm />
+    </>
   );
 }
